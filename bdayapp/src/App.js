fd0096bda53bc63244baa8652e79ba63f87c932a
Richard Peterson <webbuyrich@gmail.com>
2022-08-16 17:06:51 -0500

    added default template structure

--- bdayapp/src/App.js
+++ bdayapp/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  useState
+} from 'react';
+import data from './data';
+import List from './List';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [people, setPeople] = useState()
+
+  return(
+    <main>
+      <section className='container'>
+        <h3>0 birthdays</h3>
+        <List />
+        <button onClick={()=> console.log('you clicked me')}>Clear All</button>
+      </section>
+    </main>
+  )
 }
 
 export default App;
