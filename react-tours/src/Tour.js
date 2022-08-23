3b7a74d51d923a85ef2aed54be42c6abec4ecb9a
Richard Peterson <webbuyrich@gmail.com>
2022-08-23 18:28:18 -0500

    added loading text

---
+++ react-tours/src/Tour.js
@@ -0,0 +1,9 @@
+import React, { useState }from 'react'
+
+const Tour = () => {
+  return (
+    <h2>single tour component</h2>
+  )
+}
+
+export default Tour
---
+++ react-tours/src/Tours.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Tour from './Tour'
+
+const Tours = () => {
+  return (
+    <h2>Tours component</h2>
+  )
+}
+
+export default Tours
