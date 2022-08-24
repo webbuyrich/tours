6ef2bb787470831de80a2dcf230a40d3bf3c168f
Richard Peterson <webbuyrich@gmail.com>
2022-08-24 14:18:59 -0500

    add conditional to detect if readMore is false, then display partial text

--- react-tours/src/Tour.js
+++ react-tours/src/Tour.js
@@ -1,6 +1,10 @@
 import React, { useState }from 'react'
 
 const Tour = ({ id, image, info, price, name }) => {
+
+  // create state for readMore
+  const[readMore, setReadMore] = useState(false)
+
   return (
     <article className='single-tour'>
       <img src={image} alt={name} />
@@ -9,7 +13,9 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+        {/*set conditional to display full text or partial text*/}
+        <p>{ readMore ? info : `${info.substring(0, 200)}...
+        `}</p>
         <button className='delete-btn'>Not Intertested</button>
       </footer>
     </article>
