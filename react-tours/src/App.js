875fc241a2541d21c38152eb4ff0a77d0d618f4f
Richard Peterson <webbuyrich@gmail.com>
2022-08-24 15:35:22 -0500

    added refresh tours functionality

--- react-tours/src/App.js
+++ react-tours/src/App.js
@@ -43,6 +43,18 @@ const App = () => {
     )
   }
 
+  // check if there are any tours being displayed
+  if(tours.length === 0 ){
+    return(
+      <main>
+        <div className="title">
+          <h2>No Tours Left</h2>
+          <button className='btn' onClick={fetchTours}>Refresh</button>
+        </div>
+      </main>
+    )
+  }
+
   return <main><Tours tours={tours} removeTour={removeTour} /></main>
 }
 
